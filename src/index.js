7ef0dd17eedf7310c629882d7325c05fd87da6af
Robert Lazić <robert.lazicpz@gmail.com>
2023-04-12 14:40:30 +0200

    new file:   public/images/image1.jpg 	new file:   public/images/image2.jpg 	new file:   public/images/image3.jpg 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const books = [
+  {
+    title: 'Outlive: The Science and Art of Longevity',
+    author: 'Bill Gifford',
+    img: './images/image1.jpg',
+    id: 1,
+  },
+  {
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    author: 'James Clear',
+    img: './images/image2.jpg',
+    id: 2,
+  },
+  {
+    title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
+    author: 'Dav Pilkey',
+    img: './images/image3.jpg',
+    id: 3,
+  },
+];
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  return <h1>events</h1>;
+};
+
+const Book = ({ title, author, img }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
